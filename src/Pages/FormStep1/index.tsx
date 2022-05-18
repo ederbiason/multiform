052002664456b9fa63428aecd21e2fbdb0ff7239
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-05-18 16:18:33 -0300

    form step 1

--- src/Pages/FormStep1/index.tsx
+++ src/Pages/FormStep1/index.tsx
@@ -1,9 +1,61 @@
+import { useNavigate } from 'react-router-dom';
+
 import * as C from './styles';
 
+import { Theme } from '../../components/Theme';
+import { FormActions, useForm } from '../../contexts/FormContext';
+import { ChangeEvent, useEffect } from 'react';
+
 export function FormStep1() {
+    const navigate = useNavigate();
+
+    const { state, dispatch } = useForm();
+
+    useEffect(() => {
+        dispatch({
+            type: FormActions.setCurrentStep,
+            payload: 1
+        })
+    }, [dispatch])
+
+    function handleNextStep() {
+        if(state.name !== '') {
+            navigate('/step2');
+        } else {
+            alert('Preencha os dados.')
+        }
+    }
+
+    function handleNameChange(event: ChangeEvent<HTMLInputElement>) {
+        dispatch({
+            type: FormActions.setName,
+            payload: event.target.value
+        })
+    }
+
     return (
-        <C.Container>
-            Passo 1
-        </C.Container>
+        <Theme>
+            <C.Container>
+                <p>Passo 1/3</p>
+                <h1>Vamos começar com seu nome</h1>
+                <p>Preencha o campo abaixo com o seu nome completo.</p>
+
+                <hr />
+
+                <label>
+                    Seu nome completo
+                    <input 
+                        type="text" 
+                        autoFocus
+                        value={state.name}
+                        onChange={handleNameChange}
+                    />
+                </label>
+
+                <button onClick={handleNextStep}>
+                    Próximo
+                </button>
+            </C.Container>
+        </Theme>
     )
 }
